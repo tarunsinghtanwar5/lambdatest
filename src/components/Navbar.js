98198ecfd7ac851ada0e3c7ec97d91c7a6b1fe16
Tarun Singh Tanwar <tarunsinghtanwar14@example.com>
2021-10-21 23:54:05 +0530

    added navbar margins

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ import upgrade from '../assets/navbar/upgrade.png'
 function Navbar() {
   return (
     <div className="flex justify-end border-solid border-2 w-full filter drop-shadow-lg">
-      <img src={dropdown}  className=""/>
-      <button className=" rounded-full border-solid border-2 pl-4 pr-4 pt-1 pb-1 ">Configure Tunnel</button>
-      <button className="rounded-full border-solid border-2 pl-4 pr-4 pt-1 pb-1 bg-blue-400">Upgrade</button>
-      <img src={notification} className="" />
-      <img src={user} className=" " />
+      <img src={dropdown}  className="m-2"/>
+      <button className="m-2 rounded-full border-solid border-2 pl-4 pr-4 pt-1 pb-1 ">Configure Tunnel</button>
+      <button className="m-2 rounded-full border-solid border-2 pl-4 pr-4 pt-1 pb-1 bg-blue-400">Upgrade</button>
+      <img src={notification} className="m-2" />
+      <img src={user} className="m-1" />
     </div>
   )
 }
